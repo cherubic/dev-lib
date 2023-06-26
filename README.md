3585eb59e4e53b6a66a879e91b8c5cb3e56061b0
cherubic <copyrightcherubic@gmail.com>
2023-06-26 23:31:24 +0800

    [feat] commit shell
    1. add commit lint setup shell

--- README.md
+++ README.md
@@ -12,7 +12,14 @@
 
 相关工具：
 
-- git hook: [website](https://git-scm.com/book/zh/v2/%E8%87%AA%E5%AE%9A%E4%B9%89-Git-Git-%E9%92%A9%E5%AD%90)
-- commitlint: [website](https://commitlint.js.org/#/), [github](https://github.com/conventional-changelog/commitlint)
-- Commitizen: [website](http://commitizen.github.io/cz-cli/), [github](http://commitizen.github.io/cz-cli/)
-- husky: [website](https://typicode.github.io/husky/#/), [github](https://github.com/typicode/husky)
+- git hook: [website](https://git-scm.com/book/zh/v2/%E8%87%AA%E5%AE%9A%E4%B9%89-Git-Git-%E9%92%A9%E5%AD%90)；git操作的hook，分别有 `pre-commit`(提交前), `prepare-commit-msg`(编辑提交信息前), `commit-msg`(编辑提交信息后), `pre-receive`(推送接收到信息前), `update`(更新引用前，一般用于服务端触发), `pre-push`(推送前), `post-update`(引用更新后)
+- commitlint: [website](https://commitlint.js.org/#/), [github](https://github.com/conventional-changelog/commitlint)；校验提交消息的工具
+- Commitizen: [website](http://commitizen.github.io/cz-cli/), [github](http://commitizen.github.io/cz-cli/)；提交代码时强制执行规范化的提交消息格式
+- cz-customizable(可选): [github](https://github.com/leoforfree/cz-customizable)；提交代码时强制执行规范化的提交消息格式
+- husky: [website](https://typicode.github.io/husky/#/), [github](https://github.com/typicode/husky)；git hook管理工具
+
+
+相关脚本
+
+- commit-init.sh (Linux/Mac脚本)
+- commit-init.ps1 (Windows脚本)
---
+++ commit-init.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -x
+
+# This script is used to init git repository commit hooks tools and config.
+function check_pre_tools() {
+    command_name=$1
+    if ! command -v $command_name &> /dev/null
+    then
+        echo "$command_name could not be found"
+        exit 1
+    fi
+}
+
+function check_tools() {
+    command_name=$1
+    if ! command -v $command_name &> /dev/null
+    then
+        echo "$command_name could not be found"
+        return 1
+    else
+        echo "$command_name has been installed"
+        return 0
+    fi
+}
+
+function install_husky() {
+    pushd .
+    echo "install husky by npm"
+    if check_tools "husky"; then
+        echo "husky has been installed"
+    else
+        echo "install husky"
+        npx husky-init && npm install
+    fi
+    popd || return
+}
+
+function install_commitlint() {
+    pushd .
+    echo "install commitlint by npm"
+    npm install --save-dev @commitlint/{config-conventional,cli}
+    popd || return
+}
+
+function install_commitizen() {
+    pushd .
+    echo "install commitizen by npm"
+    ! check_tools commitizen && npm install -g commitizen
+    commitizen init cz-conventional-changelog --save-dev --save-exact
+    popd || return
+}
+
+function setup_commithook() {
+    pushd .
+    echo "setup commit hook"
+    npx husky add .husky/commit-msg "npx --no-install commitlint --config commitlint.config.yaml --edit \${1}"
+    popd || return
+}
+
+function main() {
+    check_pre_tools "git"
+    check_pre_tools "node"
+
+    pushd .
+    test ! -f "package.json" && npm init -y
+    popd || return
+
+    install_husky
+    install_commitlint
+    install_commitizen
+
+    setup_commithook
+}
+
+main "$@"
